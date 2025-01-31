a5e0bb15c911bcc70d5b4839c5f038471e54fb0a
Jad Ghalayini <jad.ghalayini@protonmail.com>
2025-01-31 20:22:37 +0000

    Updated to lean v4.16.0-rc2, batteries

--- lakefile.lean
+++ lakefile.lean
@@ -5,14 +5,12 @@ package Straume
 
 lean_lib Straume
 
-require std from git
-  "https://github.com/leanprover/std4" @ "v4.4.0"
+require "leanprover-community" / "batteries" @ git "main"
 
-require LSpec from git
-  "https://github.com/lurk-lab/LSpec" @ "3388be5a1d1390594a74ec469fd54a5d84ff6114"
+require "lurk-lab" / "LSpec" @ git "7f2c46b"
 
 require YatimaStdLib from git
-  "https://github.com/lurk-lab/YatimaStdLib.lean" @ "3037f0c14128751b95510c2723f067ec7a494f08"
+  "https://github.com/imbrem/YatimaStdLib.lean" @ "main"
 
 @[default_target]
 lean_exe straume where
